ad025ec1c293a3dedd26696b1319619934d1210a
Glodenox <github@tomputtemans.com>
2019-02-25 22:04:38 +0100

    Allow zooming in further on the report page

--- js/reports.js
+++ js/reports.js
@@ -942,7 +942,7 @@ Dashboard.ReportView.prototype.refresh = function() {
 				'https://worldtiles4.waze.com/tiles/${z}/${x}/${y}.png'
 			], {
 				projection: mapProjection,
-				numZoomLevels: 18,
+				numZoomLevels: 23,
 				attribution: '&copy; 2006-' + (new Date()).getFullYear() + ' <a href="https://www.waze.com/livemap" target="_blank">Waze Mobile</a>. All Rights Reserved.'
 			})
 		],
